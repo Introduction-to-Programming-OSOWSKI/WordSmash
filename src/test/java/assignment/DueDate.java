2f0e1603467491670847bed098ad74db9695d99a
zackosowski <ZACHARYO@MYSTMA.ORG>
2020-11-24 19:58:46 +0000

    Added dude date

---
+++ src/test/java/assignment/DueDate.java
@@ -0,0 +1,15 @@
+package assignment;
+
+import java.util.Calendar;
+
+public class DueDate {
+    
+
+    public static boolean onTime(int year, int month, int day){
+    Calendar cal = Calendar.getInstance();
+    Calendar due = Calendar.getInstance();
+    due.set(year, month - 1, day, 22, 0);
+    return cal.getTime().compareTo(due.getTime()) < 0;
+    }
+    
+}
--- src/test/java/assignment/Tests.java
+++ src/test/java/assignment/Tests.java
@@ -1,6 +1,7 @@
 package assignment;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 //import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,10 +15,16 @@ public class Tests {
 
 
    @Test
-   public void test(){
+   public void code(){
        App.main(null);
        assertEquals("yesno", App.wordSmash("yes", "no"), "ERROR MESSAGE");
        assertEquals("five two", App.wordSmash("five ", "two"), "ERROR MESSAGE");
    }
+
+   @Test
+    public void late() {
+        App.main(null);
+        assertTrue(DueDate.onTime(2020, 12, 16), "Submitted Late");
+    }
 
 }
